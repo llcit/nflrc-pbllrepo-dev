442d549cb4f9183b8660d195cd42e222a7d17059
Richard Medina <richmedina@gmail.com>
2016-11-30 00:36:28 -0700

    index installed

--- pbllrepo/settings/dev.py
+++ pbllrepo/settings/dev.py
@@ -24,17 +24,7 @@ DATABASES = {
     # }
 }
 
-INSTALLED_APPS += ( 
-    'haystack',
-)
 
-HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'pbllrepo-haystack',
-    },
-}
 
 STATIC_ROOT = '/Library/WebServer/Documents/static/pbllrepo'
 STATIC_URL = '/static/'
